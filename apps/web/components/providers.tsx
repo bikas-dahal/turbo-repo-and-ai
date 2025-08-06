0e8e19918780dd69dc7e1c8c6d1a1e6c7e1aa460
Bikas Dahal <bikkydahal@gmail.com>
2025-08-06 08:03:31 +0545

    added clerk for auth

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -3,6 +3,12 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { ConvexProvider, ConvexReactClient } from "convex/react"
+import { ConvexProviderWithClerk } from "convex/react-clerk"
+import  { useAuth }from '@clerk/nextjs'
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+    throw new Error("NEXT_PUBLIC_CONVEX_URL is not defined")
+}
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
@@ -15,7 +21,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
       disableTransitionOnChange
       enableColorScheme
     >
-      <ConvexProvider client={convex}>{children}</ConvexProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
     </NextThemesProvider>
   )
 }
